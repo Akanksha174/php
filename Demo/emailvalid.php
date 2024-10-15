2724067dd768a201a0b4e434ee4256c1e6b30962
Akanksha174 <akankshakumari4755@gmail.com>
2024-10-15 10:43:50 +0530

    updates

---
+++ Demo/emailvalid.php
@@ -0,0 +1,16 @@
+<?php
+
+    if(isset($_POST['submit'])){
+        $name1 = $_POST['name1'];
+        $name2 = $_POST['name2'];
+        $institute = $_POST['inst'];
+        $email = $_POST['email'];
+        $txt = $_POST['area'];
+    }
+
+    echo "Full name : {$name1} {$name2} <br>";
+    echo "Institute : {$institute}<br>";
+    echo "Email : {$email}<br>";
+    echo "Name of the organization : {$txt} <br>";
+
+?>
